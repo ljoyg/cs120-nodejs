2c0a4a71d4e5c21245107a6b6c8b83dfce0537d9
ljoyg <114941471+ljoyg@users.noreply.github.com>
2026-04-28 19:37:46 -0400

    updating app files

---
+++ app.js
@@ -0,0 +1,45 @@
+const MongoClient = require('mongodb').MongoClient;
+const readline = require('node:readline');
+
+const url = "mongodb+srv://ljgaither99_db_user:WyuA6p3uQv88YLfP@hw10.0ibs88j.mongodb.net/?appName=hw10";
+
+const isDigit = (char) => /^\d$/.test(char);
+
+async function startServer() {
+  http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  if (req.url == "/")
+    {
+      s = '<div id="display">' +
+          '<form id="myForm" action="/process" method="post">' +
+              '<label>Please enter a zip code or a place: </label><input type="text" id="answer"><br/>' +
+              '<input type="submit">' +
+          '</form>' +
+      '</div>';
+      res.write(s);
+    }
+  else if (req.url == "/process") {
+    try {
+        var body = '';
+        req.on('data', chunk => { body += chunk.toString(); });
+        var search = qs.parse(body).answer;
+        MongoClient.connect(url, async function(err, db) {
+            var dbo = db.db("hw10");
+            var collection = dbo.collection('places');
+            if(err) { console.log(err); }
+            else {
+                if (isDigit(search[0])) {
+                    const results = await collection.find({ zips: val });
+                } else {
+                    const results = await collection.find({ place: val })
+                };
+                res.write(results);
+            };
+        });
+    } finally {
+        db.close();
+    }
+  }
+  res.end();
+}).listen(3000);
+}

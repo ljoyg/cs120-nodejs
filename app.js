767d6f27235697e479c3e64f5a0b10c303776273
ljoyg <114941471+ljoyg@users.noreply.github.com>
2026-04-28 19:58:23 -0400

    simplified attempt

--- app.js
+++ app.js
@@ -8,15 +8,15 @@ const mongourl = "mongodb+srv://ljgaither99_db_user:WyuA6p3uQv88YLfP@hw10.0ibs88
 
 const isDigit = (char) => /^\d$/.test(char);
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8080;
 
 async function startServer() {
   http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   urlObj = mongourl.parse(req.url,true)
   path = urlObj.pathname;
-  if (path)
-    {
+//   if (path == "/")
+//     {
       s = '<div id="display">' +
           '<form id="myForm" action="/process" method="post">' +
               '<label>Please enter a zip code or a place: </label><input type="text" id="answer"><br/>' +
@@ -24,29 +24,29 @@ async function startServer() {
           '</form>' +
       '</div>';
       res.write(s);
-    }
-  else if (path == "/process") {
-    try {
-        var body = '';
-        req.on('data', chunk => { body += chunk.toString(); });
-        var search = qs.parse(body).answer;
-        MongoClient.connect(url, async function(err, db) {
-            var dbo = db.db("hw10");
-            var collection = dbo.collection('places');
-            if(err) { console.log(err); }
-            else {
-                if (isDigit(search[0])) {
-                    const results = await collection.find({ zips: val });
-                } else {
-                    const results = await collection.find({ place: val })
-                };
-                res.write(results);
-            };
-        });
-    } finally {
-        db.close();
-    }
-  }
+//     }
+//   else if (path == "/process") {
+//     try {
+//         var body = '';
+//         req.on('data', chunk => { body += chunk.toString(); });
+//         var search = qs.parse(body).answer;
+//         MongoClient.connect(url, async function(err, db) {
+//             var dbo = db.db("hw10");
+//             var collection = dbo.collection('places');
+//             if(err) { console.log(err); }
+//             else {
+//                 if (isDigit(search[0])) {
+//                     const results = await collection.find({ zips: val });
+//                 } else {
+//                     const results = await collection.find({ place: val })
+//                 };
+//                 res.write(results);
+//             };
+//         });
+//     } finally {
+//         db.close();
+//     }
+//   }
   res.end();
 }).listen(port);
 }

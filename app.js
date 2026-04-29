2ae43cfc77f51577a52ba9cb334618328476c669
ljoyg <114941471+ljoyg@users.noreply.github.com>
2026-04-28 21:32:13 -0400

    new strategy

--- app.js
+++ app.js
@@ -29,76 +29,39 @@ http.createServer(function (req, res) {
     req.on('end', () => 
         { 
         res.write ("Raw data string: " + body +"<br/>");
-        try {
-            var search = qs.parse(body).answer;
-            MongoClient.connect(mongourl, async function(err, db) {
-                console.log("connected");
+        var search = qs.parse(body).answer;
+        async function searchDB(search) {
+            try {
+                await client.connect();
                 var dbo = db.db("hw10");
                 var collection = dbo.collection('places');
                 if(err) { console.log(err); }
-                // else {
-                //     if (isDigit(search[0])) {
-                //         const results = await collection.find({ zips: val });
-                //     } else {
-                //         const results = await collection.find({ place: val })
-                //     };
-                //     res.write(results);
-                // };
-            });
-        } finally {
-            db.close();
-            res.end();
-        }
+                console.log("connected");
+            } finally {
+                db.close();
+                res.end();
+            }
+        };
+        // try {
+        //     MongoClient.connect(mongourl, async function(err, db) {
+        //         console.log("connected");
+        //         var dbo = db.db("hw10");
+        //         var collection = dbo.collection('places');
+        //         if(err) { console.log(err); }
+        //         // else {
+        //         //     if (isDigit(search[0])) {
+        //         //         const results = await collection.find({ zips: search });
+        //         //     } else {
+        //         //         const results = await collection.find({ place: search })
+        //         //     };
+        //         //     res.write(results);
+        //         // };
+        //     });
+        // } finally {
+        //     db.close();
+        //     res.end();
+        // }
     });
   }
 }).listen(port);
-
-
-// var http = require('http');
-// const { start } = require('node:repl');
-// // var url = require('url');
-// // require("node:dns/promises").setServers(["1.1.1.1", "8.8.8.8", "0.0.0.0"]);
-// // const MongoClient = require('mongodb').MongoClient;
-// // const readline = require('node:readline');
-
-// // const mongourl = "mongodb+srv://ljgaither99_db_user:WyuA6p3uQv88YLfP@hw10.0ibs88j.mongodb.net/?appName=hw10";
-
-// const isDigit = (char) => /^\d$/.test(char);
-
-// var port = process.env.PORT || 3000;
-
-// async function startServer() {
-//     console.log("i'm trying my best");
-//     http.createServer(function (req, res) {
-//     res.writeHead(200, {'Content-Type': 'text/html'});
-// //   if (path == "/")
-// //     {
-// //     }
-// //   else if (path == "/process") {
-// //     try {
-// //         var body = '';
-// //         req.on('data', chunk => { body += chunk.toString(); });
-// //         var search = qs.parse(body).answer;
-// //         MongoClient.connect(url, async function(err, db) {
-// //             var dbo = db.db("hw10");
-// //             var collection = dbo.collection('places');
-// //             if(err) { console.log(err); }
-// //             else {
-// //                 if (isDigit(search[0])) {
-// //                     const results = await collection.find({ zips: val });
-// //                 } else {
-// //                     const results = await collection.find({ place: val })
-// //                 };
-// //                 res.write(results);
-// //             };
-// //         });
-// //     } finally {
-// //         db.close();
-// //     }
-// //   }
-//   res.end();
-// }).listen(port);
-// }
-
-// startServer();
 
